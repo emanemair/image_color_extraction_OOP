44aec06379d577f4804d697996c0099caab3500c
emanimair <emanimair2@gmail.com>
2023-06-21 11:40:08 +0300

    adding main file

---
+++ main.py
@@ -0,0 +1,10 @@
+from colorextraction import ColorExtraction
+from displaycolor import Display
+number_of_color=int(input("enter number of color : 1-15"))
+color_extraction = ColorExtraction("eman.jpg" , number_of_color)
+colors =color_extraction.colors_to_rgb()
+color_file = open("color.txt" , "w")
+color_file.write(str(colors))
+color_file.close()
+screen=Display(colors)
+screen.draw()
